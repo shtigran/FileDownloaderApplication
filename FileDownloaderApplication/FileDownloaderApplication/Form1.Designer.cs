994302959e43f0e11e7959793d7d6a3dba73157f
shtigran <shakhbekyantigran@gmail.com>
2017-02-12 16:41:28 +0400

    Add textbox

--- FileDownloaderApplication/FileDownloaderApplication/Form1.Designer.cs
+++ FileDownloaderApplication/FileDownloaderApplication/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.InputUrl = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputUrl
+            // 
+            this.InputUrl.Location = new System.Drawing.Point(35, 73);
+            this.InputUrl.Multiline = true;
+            this.InputUrl.Name = "InputUrl";
+            this.InputUrl.Size = new System.Drawing.Size(175, 20);
+            this.InputUrl.TabIndex = 0;
+            this.InputUrl.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.InputUrl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox InputUrl;
     }
 }
 
--- FileDownloaderApplication/FileDownloaderApplication/Form1.cs
+++ FileDownloaderApplication/FileDownloaderApplication/Form1.cs
@@ -16,5 +16,10 @@ namespace FileDownloaderApplication
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
