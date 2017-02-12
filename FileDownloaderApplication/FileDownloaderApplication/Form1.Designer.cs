7210913d86e71c673271b1f2c4d9b1bf53921037
shtigran <shakhbekyantigran@gmail.com>
2017-02-12 16:43:24 +0400

    Add button search

--- FileDownloaderApplication/FileDownloaderApplication/Form1.Designer.cs
+++ FileDownloaderApplication/FileDownloaderApplication/Form1.Designer.cs
@@ -29,22 +29,33 @@
         private void InitializeComponent()
         {
             this.InputUrl = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // InputUrl
             // 
-            this.InputUrl.Location = new System.Drawing.Point(35, 73);
+            this.InputUrl.Location = new System.Drawing.Point(12, 71);
             this.InputUrl.Multiline = true;
             this.InputUrl.Name = "InputUrl";
-            this.InputUrl.Size = new System.Drawing.Size(175, 20);
+            this.InputUrl.Size = new System.Drawing.Size(213, 32);
             this.InputUrl.TabIndex = 0;
             this.InputUrl.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(251, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(363, 310);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.InputUrl);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox InputUrl;
+        private System.Windows.Forms.Button button1;
     }
 }
 
