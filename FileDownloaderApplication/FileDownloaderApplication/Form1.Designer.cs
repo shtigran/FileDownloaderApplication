994df9581eb273b27f2dc41d81475bf00143c61f
shtigran <shakhbekyantigran@gmail.com>
2017-02-12 16:59:54 +0400

    Add url label and group

--- FileDownloaderApplication/FileDownloaderApplication/Form1.Designer.cs
+++ FileDownloaderApplication/FileDownloaderApplication/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FileDownloaderApplication
 {
-    partial class Form1
+    partial class FileDownloader
     {
         /// <summary>
         /// Required designer variable.
@@ -30,37 +30,68 @@
         {
             this.InputUrl = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // InputUrl
             // 
-            this.InputUrl.Location = new System.Drawing.Point(12, 71);
+            this.InputUrl.AcceptsTab = true;
+            this.InputUrl.AccessibleRole = System.Windows.Forms.AccessibleRole.Cursor;
+            this.InputUrl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputUrl.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.AllUrl;
+            this.InputUrl.Location = new System.Drawing.Point(63, 36);
             this.InputUrl.Multiline = true;
             this.InputUrl.Name = "InputUrl";
-            this.InputUrl.Size = new System.Drawing.Size(213, 32);
+            this.InputUrl.Size = new System.Drawing.Size(235, 32);
             this.InputUrl.TabIndex = 0;
             this.InputUrl.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(251, 71);
+            this.button1.Location = new System.Drawing.Point(195, 74);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 32);
+            this.button1.Size = new System.Drawing.Size(103, 27);
             this.button1.TabIndex = 1;
             this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.InputUrl);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(107, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(304, 145);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter URL";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "URL:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FileDownloader
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(363, 310);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.InputUrl);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(433, 288);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FileDownloader";
+            this.Text = "FileDownloader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -68,6 +99,8 @@
 
         private System.Windows.Forms.TextBox InputUrl;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- FileDownloaderApplication/FileDownloaderApplication/Form1.cs
+++ FileDownloaderApplication/FileDownloaderApplication/Form1.cs
@@ -10,14 +10,29 @@ using System.Windows.Forms;
 
 namespace FileDownloaderApplication
 {
-    public partial class Form1 : Form
+    public partial class FileDownloader : Form
     {
-        public Form1()
+
+
+
+
+
+        public FileDownloader()
         {
             InitializeComponent();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Scanning of " + InputUrl.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
